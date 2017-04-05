256ffc00f750a65ed7ddf6ab8bee99b66e984fee
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-04-05 13:22:53 +0530

    made changes in switch css js file and added webpage_stripper js file

--- switch_css.js
+++ switch_css.js
@@ -1,45 +1,45 @@
 (function alternate_stylesheets(){
+        //appending a CSS alternate stylesheets to head of webpage
 	var i= 0;
-	var css_links = 3;
-	var link_rel =['stylesheet', 'alternate stylesheet', 'alternate stylesheet'] 
-	var css_themes =['https://rawgit.com/sadhanareddy/css-changer-tool/master/switch1.css',
-	'https://rawgit.com/sadhanareddy/css-changer-tool/master/switch2.css',
-	'https://rawgit.com/sadhanareddy/css-changer-tool/master/switch3.css'];
+	var style_sheets = 3; 
+	var link_rel =['alternate stylesheet', 'alternate stylesheet', 'alternate stylesheet'] 
+	var css_themes =['https://cdn.rawgit.com/renarration-studio/webpage-transformation/master/switch1.css',
+	'https://cdn.rawgit.com/renarration-studio/webpage-transformation/master/switch2.css',
+	'https://cdn.rawgit.com/renarration-studio/webpage-transformation/master/switch3.css'];
 	var link_title =['switch1', 'switch2', 'switch3'];
 
-	for(i=0; i<css_links; i++){
-	    link = document.createElement('link');
-	    link.rel  = link_rel[i];
-	    link.type = 'text/css';
-	    link.href = css_themes[i];
-	    link.title= link_title[i];
+	for(i=0; i<style_sheets; i++){
+	    var linktag = document.createElement('link');
+	    linktag.rel  = link_rel[i];
+	    linktag.type = 'text/css';
+	    linktag.href = css_themes[i];
+	    linktag.title= link_title[i];
 	    head  = document.getElementsByTagName('head')[0];
-	    head.appendChild(link);
+	    head.appendChild(linktag);
 	}
 
 }());
 (function switchcss_container(){
     //appending a CSS stylesheet to head of webpage
-    var link = document.createElement('link');
-    // using rawgit.com MaxCDN.. files directly linked to git repo 'annoletjs/master'
-    link.rel = "stylesheet";
-    link.type = "text/css";
-    link.href = "https://rawgit.com/sadhanareddy/css-changer-tool/master/annolet.css"; //random version number removed bcoz some browser take it as text file and not as CSS.
-    document.getElementsByTagName('head')[0].appendChild(link);
+    var linktag = document.createElement('link');
+    linktag.rel = "stylesheet";
+    linktag.type = "text/css";
+    //using rawgit.com MaxCDN.. files directly linked to git repo 'webpage-transformation/master'
+    linktag.href = "https://cdn.rawgit.com/renarration-studio/webpage-transformation/master/annolet.css"; //random version number removed bcoz some browser take it as text file and not as CSS.
+    document.getElementsByTagName('head')[0].appendChild(linktag);
     // appending a div to body of webpage
     var body = document.getElementsByTagName('body')[0];
-    var switchcss_container = document.createElement('div');
-    switchcss_container.id = 'switchcss-container';
-    body.appendChild(switchcss_container);
-
+    var container = document.createElement('div');
+    container.id = 'switchcss-container';
+    body.appendChild(container);
     //appending buttons into the div inner HTML
 	var i= 0;
 	var buttons = 3;
 	var text_node = ['Theme_1', 'Theme_2', 'Theme_3'];
 	var button_id = ['theme_1', 'theme_2','theme_3'];
 	for(i=0; i<buttons; i++){
-		button_tag = document.createElement("BUTTON");
-		text = document.createTextNode(text_node[i]);
+		var button_tag = document.createElement("BUTTON");
+		var text = document.createTextNode(text_node[i]);
 		button_tag.id = button_id[i];
 		button_tag.appendChild(text);
 		document.getElementById('switchcss-container').appendChild(button_tag);
@@ -61,8 +61,7 @@ function switch_style (css_title)
 	alert(css_title);
   	var i;
   	var linktag = document.getElementsByTagName("link");
-
-  	for (i = 0; i < linktag.length; i++ ) {
+	for (i = 0; i < linktag.length; i++ ) {
 		if ((linktag[i].rel.indexOf( "stylesheet" ) != -1) &&linktag[i].title) {
 		  	linktag[i].disabled = true ;
 		  	if (linktag[i].title == css_title) {
