8056ffa265a52fb7d2022e28d722648b88c3a1b7
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-04-06 11:15:53 +0530

    add css

--- lang_translation.js
+++ lang_translation.js
@@ -1,11 +1,17 @@
 (function switchcss_container(){
     //appending a CSS stylesheet to head of webpage
     var link = document.createElement('link');
-    // using rawgit.com MaxCDN.. files directly linked to git repo 'annoletjs/master'
+    // using rawgit.com MaxCDN.. files directly linked to git repo 'webpage-transformation/master'
     link.rel = "stylesheet";
     link.type = "text/css";
-    link.href = "https://rawgit.com/sadhanareddy/css-changer-tool/master/annolet.css"; //random version number removed bcoz some browser take it as text file and not as CSS.
+    link.href = "https://cdn.rawgit.com/renarration-studio/webpage-transformation/2b3df055c68d7bb024d13929aad43ea67ff5f8a9/css/main.css"; //random version number removed bcoz some browser take it as text file and not as CSS.
     document.getElementsByTagName('head')[0].appendChild(link);
+    //appending h2 tag into the div inner HTML
+    var header = document.createElement('h2');
+    var text = document.createTextNode("Language Translation");
+    header.id = 'themes-header';
+    header.appendChild(text);
+    document.getElementById('switchcss-container').appendChild(header);
     // appending a div to body of webpage
     var body = document.getElementsByTagName('body')[0];
     var switchcss_container = document.createElement('div');
@@ -16,14 +22,25 @@
     select_list()
     create_textArea()
 }());
+
+(function googleTrans_Api() {
+    //appending a script tag to head of webpage
+    var script = document.createElement('script');
+    script.type = "text/javascript";
+    script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+    document.getElementsByTagName('head')[0].appendChild(script);
+}());
+
 function googleTranslateElementInit() {
    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
 }
+
 function create_div(){
      div = document.createElement('div');
      div.id = 'google_translate_element';
      document.getElementById('switchcss-container').appendChild(div);
 }
+
 function Translate_button(){
      button = document.createElement("BUTTON");
      var text = document.createTextNode("Translate");
@@ -31,6 +48,7 @@ function Translate_button(){
      button.appendChild(text);
      document.getElementById('switchcss-container').appendChild(button);
 }
+
 function select_list(){
       //Create array of options to be added
       var array = ["telugu","hindi", "malayalam"];
@@ -48,6 +66,7 @@ function select_list(){
           selectList.appendChild(option);
       }
 }
+
 function create_textArea() {
     var output = document.createElement("TEXTAREA");
     output.id="textarea";
@@ -55,6 +74,7 @@ function create_textArea() {
     output.appendChild(t);
     document.getElementById('switchcss-container').appendChild(output);
 }
+
 (function languageTrans_API(){
   var url = "https://translate.yandex.net/api/v1.5/tr.json/translate",
   keyAPI = "trnsl.1.1.20170315T015859Z.3e04bd9bd31f6f00.99aa35ddf89167a86f5a892014edf632e9cef14f";
