52cbe8cc7c67d7f2ac35ea0be791883662d661ac
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-05-10 11:58:37 +0530

    modified transformation js file

--- transformation_tool.js
+++ transformation_tool.js
@@ -19,6 +19,36 @@
     linktag.href = "https://cdn.rawgit.com/renarration-studio/webpage-transformation/77492ebc/css/annolet.css"; //random version number removed bcoz some browser take it as text file and not as CSS.
     document.getElementsByTagName('head')[0].appendChild(linktag);
 
+    // Creates dropdown box and appended into the annolet container div for switching the themes of a webpage. 
+    var themes_div = document.createElement('div');
+    themes_div.class = 'dropdown';
+    container.appendChild(themes_div);
+
+    var button_tag = document.createElement("BUTTON");
+    var text = document.createTextNode(Switch Themes);
+    var span = document.createElement("span");
+    span.class = "caret";
+    button_tag.class = "btn btn-primary dropdown-toggle";
+    button.type = "button";
+    button.setAttribute('data-toggle', 'dropdown');
+    button_tag.appendChild(text);
+    button_tag.appendChild(span);
+    themes_div.appendChild(button_tag);
+
+    var ul_list_tag = document.createElement('ul');
+    ul_list_tag.class = 'dropdown-menu';
+    themes_div.appendChild(ul_list_tag);
+    var i= 0;
+    var list_elements = 3;
+    var text_node = ['Theme 1', 'Theme 2', 'Theme 3'];
+    var list_elements_id = ['theme-1', 'theme-2','theme-3'];
+    for(i=0; i<list_elements; i++){
+        var list_elements_tag = document.createElement("li");
+        var text = document.createTextNode(text_node[i]);
+        list_elements_tag.id = list_elements_id[i];
+        list_elements_tag.appendChild(text);
+        ul_list_tag.appendChild(list_elements_tag);
+    }
     // //appending buttons into the div inner HTML
     // var i= 0;
     // var buttons = 3;
