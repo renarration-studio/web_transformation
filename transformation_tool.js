4fd8a8dfe0c3529fea720b0b24aa4154c01bca08
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-05-10 14:04:25 +0530

    modified transformation js file

--- transformation_tool.js
+++ transformation_tool.js
@@ -20,20 +20,20 @@
     document.getElementsByTagName('head')[0].appendChild(linktag);
 
     // Creates dropdown box and appended into the annolet container div for switching the themes of a webpage. 
-    var themes_div = document.createElement('div');
-    themes_div.className = 'dropdown';
-    container.appendChild(themes_div);
+    // var themes_div = document.createElement('div');
+    // themes_div.className = 'dropdown';
+    // container.appendChild(themes_div);
 
-    var button_tag = document.createElement("BUTTON");
-    var text = document.createTextNode(Switch Themes);
+    // var button_tag = document.createElement("BUTTON");
+    // var text = document.createTextNode(SwitchThemes);
     // var span = document.createElement("span");
     // span.className = "caret";
-    button_tag.className = "btn btn-primary dropdown-toggle";
-    button.type = "button";
-    button.setAttribute('data-toggle', 'dropdown');
-    button_tag.appendChild(text);
+    // button_tag.className = "btn btn-primary dropdown-toggle";
+    // button.type = "button";
+    // button.setAttribute('data-toggle', 'dropdown');
+    // button_tag.appendChild(text);
     // button_tag.appendChild(span);
-    themes_div.appendChild(button_tag);
+    // themes_div.appendChild(button_tag);
 
     // var ul_list_tag = document.createElement('ul');
     // ul_list_tag.class = 'dropdown-menu';
@@ -49,19 +49,42 @@
     //     list_elements_tag.appendChild(text);
     //     ul_list_tag.appendChild(list_elements_tag);
     // }
-    // //appending buttons into the div inner HTML
-    // var i= 0;
-    // var buttons = 3;
-    // var text_node = ['Theme 1', 'Theme 2', 'Theme 3'];
-    // var button_id = ['theme-1', 'theme-2','theme-3'];
-    // for(i=0; i<buttons; i++){
-    //      var button_tag = document.createElement("BUTTON");
-    //      var text = document.createTextNode(text_node[i]);
-    //      button_tag.id = button_id[i];
-    //      button_tag.className = "annolet-elements";
-    //      button_tag.appendChild(text);
-    //      document.getElementById('annolet-container').appendChild(button_tag);
-    // }
+
+    // Creates dropdown box and appended into the annolet container div for switching the themes of a webpage. 
+    var themes_div = document.createElement('div');
+    themes_div.className = 'dropdown';
+    container.appendChild(themes_div);
+
+    var button_tag = document.createElement('BUTTON');
+    var text = document.createTextNode('SwitchThemes');
+    button_tag.setAttribute('class', 'dropbtn');
+    button_tag.appendChild(text);
+    themes_div.appendChild(button_tag);
+
+    var dropdown_div = document.createElement('div');
+    dropdown_div.className = 'dropdown-content';
+    themes_div.appendChild(dropdown_div);
+
+    var i= 0;
+    var list_elements = 3;
+    var text_node = ['Theme 1', 'Theme 2', 'Theme 3'];
+    var list_elements_id = ['theme-1', 'theme-2','theme-3'];
+    for(i=0; i<list_elements; i++){
+        var list_elements_tag = document.createElement("a");
+        var text = document.createTextNode(text_node[i]);
+        list_elements_tag.id = list_elements_id[i];
+        list_elements_tag.appendChild(text);
+        dropdown_div.appendChild(list_elements_tag);
+    }
+
+//   <div class="dropdown">
+//   <button class="dropbtn">Dropdown</button>
+//   <div class="dropdown-content">
+//     <a href="#">Link 1</a>
+//     <a href="#">Link 2</a>
+//     <a href="#">Link 3</a>
+//   </div>
+// </div>
 }());
 
 
