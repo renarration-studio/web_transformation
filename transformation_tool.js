d8156c9b47949c2babb5cf10850838edaae3baaa
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-05-13 23:42:48 +0530

    modified css

--- transformation_tool.js
+++ transformation_tool.js
@@ -10,7 +10,7 @@
     linktag.rel = "stylesheet";
     linktag.type = "text/css";
     //using rawgit.com MaxCDN.. files directly linked to git repo 'webpage-transformation/master'
-    linktag.href = "https://cdn.rawgit.com/renarration-studio/webpage-transformation-tool/7bdb2b0a/css/annolet.css"; 
+    linktag.href = "https://cdn.rawgit.com/renarration-studio/webpage-transformation-tool/3ef6ea6c/css/annolet.css"; 
     document.getElementsByTagName('head')[0].appendChild(linktag);
     
     // //injecting html code
@@ -19,12 +19,13 @@
         "<li id='disable-css' class='annolet-element'>Disable CSS</li>"+
         "<li id='zapper' class='annolet-element' >Zapper</li>"+
         "<li id='phonetic-trans' class='annolet-element' >Phonetics Translation</li>"+
-        "<li class='annolet-element'>"+
+        "<li>"+
             "<select class='select-menu' >"+
                 "<option value='theme1' id='theme-1' >Switch to theme1</option>"+
                 "<option value='theme2' id='theme-2' >Switch to theme2</option>"+
                 "<option value='theme3' id='theme-3' >Switch to theme3</option>"+
-            "</select>"+
+            "</select>"+"<br><br>"+
+            "<h6 style='color:white;left:5px;'>Switch CSS</h6>"+
         "</li>"+
     "</ul>";
 }());
