0f68116c61d4c2c27402d99e8323cd8b6f377e86
sadhanareddy <sadhana.virupaksha@gmail.com>
2017-05-14 09:51:13 +0530

    modified css

--- transformation_tool.js
+++ transformation_tool.js
@@ -10,7 +10,7 @@
     linktag.rel = "stylesheet";
     linktag.type = "text/css";
     //using rawgit.com MaxCDN.. files directly linked to git repo 'webpage-transformation/master'
-    linktag.href = "https://cdn.rawgit.com/renarration-studio/webpage-transformation-tool/ba27769d/css/annolet.css"; 
+    linktag.href = "https://cdn.rawgit.com/renarration-studio/webpage-transformation-tool/323b6f41/css/annolet.css"; 
     document.getElementsByTagName('head')[0].appendChild(linktag);
     
     // //injecting html code
